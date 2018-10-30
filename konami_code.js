f35621cfef835b26ed2beffe3264026a40ab8d14
Angela Branaes <angela@proximistyle.com>
2018-10-30 16:56:47 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,9 +4,6 @@ function init() {
   // Write your JavaScript code inside the init() function
   document.addEventListener('keydown', onKeyDownHandler);
 }
-
-// Key codes for A, B, and C keys.
-const alphabet = [65, 66, 67];
  
 // Keep track of index outside of the event handler.
 let index = 0;
@@ -14,11 +11,11 @@ let index = 0;
 function onKeyDownHandler(e) {
   const key = parseInt(e.detail || e.which);
   console.log(key)
-  if (key === alphabet[index]) {
+  if (key === code[index]) {
     index++;
     console.log('index is', index)
  
-    if (index === alphabet.length) {
+    if (index === code.length) {
       alert("Hurray!");
  
       index = 0;
